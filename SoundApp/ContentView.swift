076880609fd2dda4cd95e24b12634dc75ebaa1b6
landerwells <landerwells@gmail.com>
2023-05-22 13:35:48 -0500

    initial commit

--- SoundApp/ContentView.swift
+++ SoundApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, I am typing this message")
         }
         .padding()
     }
