60c3e79098fb90444d3ad6d03b05e671e7c48d05
landerwells <landerwells@gmail.com>
2023-05-22 11:14:42 -0500

    Initial Commit

---
+++ SoundApp/SoundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundAppApp.swift
+//  SoundApp
+//
+//  Created by Lander Wells on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SoundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
